3d1a878b802f864a5c031b26987fd52c004ea931
kelllly322 <kellylan322@gmail.com>
2024-04-23 13:15:04 +0800

    Add project files.

---
+++ DemoBlazorCRUDOperationsWithDotNet8/Migrations/20240406044333_Second.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoBlazorCRUDOperationsWithDotNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "Products",
+                newName: "UnitOfEachDosage");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Products",
+                newName: "Operator");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Date",
+                table: "Products",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DidOrDidntTakenMedicine",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EachDosage",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instruction",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MedicineName",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "OperationTime",
+                table: "Products",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "SymptomDescription",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DidOrDidntTakenMedicine",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "EachDosage",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Instruction",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "MedicineName",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OperationTime",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SymptomDescription",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "UnitOfEachDosage",
+                table: "Products",
+                newName: "Quantity");
+
+            migrationBuilder.RenameColumn(
+                name: "Operator",
+                table: "Products",
+                newName: "Name");
+        }
+    }
+}
---
+++ DemoBlazorCRUDOperationsWithDotNet8/Migrations/20240406084314_Third.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoBlazorCRUDOperationsWithDotNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DidOrDidntTakenMedicine",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitOfEachDosage",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SymptomDescription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DidOrDidntTakeMedicine",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DidOrDidntTakeMedicine",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UnitOfEachDosage",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SymptomDescription",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DidOrDidntTakenMedicine",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
